e9cd0cb82a2841bccbe721b89773850c4c313f88
allpau <98777999+allpau@users.noreply.github.com>
2022-04-15 13:37:02 -0400

    correct data type issues, add objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
-let pokemonList = []
+//create array of pokemon//
+let pokemonList = [];
+
+//create objects to contain pokemon data//
 let pokemon001 = {
-  name: Bulbasaur,
-  number: 001,
-  type: [Grass, Poison],
+  name: 'Bulbasaur',
+  number: 1,
+  type: ['Grass', 'Poison'],
   height: '2\' 4\"',
   weight: 15.2
 };
 
 
 let pokemon002 = {
-  name: Ivysaur,
-  number: 002,
-  type: [Grass, Poison],
+  name: 'Ivysaur',
+  number: 2,
+  type: ['Grass', 'Poison'],
   height: '3\' 3\"',
   weight: 28.7
 };
 
 let pokemon003 = {
-  name: Venussaur,
-  number: 003,
-  type: [Grass, Poison],
+  name: 'Venusaur',
+  number: 3,
+  type: ['Grass', 'Poison'],
   height: '6\' 7\"',
   weight: 220.5
 };
 
 let pokemon004 = {
-  name: Charmander,
-  number: 004,
-  type: Fire,
+  name: 'Charmander',
+  number: 4,
+  type: 'Fire',
   height: '2\'',
   weight: 18.7
 };
+
+//add pokemon objects to array//
+pokemonList=[pokemon001, pokemon002, pokemon003, pokemon004];
