13660edd142c55d29550a8229e83d9e92a338f72
allpau <98777999+allpau@users.noreply.github.com>
2022-04-15 13:20:04 -0400

    adding first 4 Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
 let pokemonList = []
+let pokemon001 = {
+  name: Bulbasaur,
+  number: 001,
+  type: [Grass, Poison],
+  height: '2\' 4\"',
+  weight: 15.2
+};
+
+
+let pokemon002 = {
+  name: Ivysaur,
+  number: 002,
+  type: [Grass, Poison],
+  height: '3\' 3\"',
+  weight: 28.7
+};
+
+let pokemon003 = {
+  name: Venussaur,
+  number: 003,
+  type: [Grass, Poison],
+  height: '6\' 7\"',
+  weight: 220.5
+};
+
+let pokemon004 = {
+  name: Charmander,
+  number: 004,
+  type: Fire,
+  height: '2\'',
+  weight: 18.7
+};
